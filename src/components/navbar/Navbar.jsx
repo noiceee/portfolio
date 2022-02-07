670638d22a7ffe0b682504f11edbdbd672a79d48
Kartikey Singh <codingwalaid@gmail.com>
2022-02-08 02:36:35 +0530

    Added some responsiveness

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,30 @@
 import "./navbar.scss";
 import logo from "./kartikey.svg";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [toggled, setToggle] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  window.onscroll = ()=>{
+    setScrolled(window.pageYOffset !== 0);
+  }
+
   return (
-    <nav className="navbar">
+    <nav className={scrolled?"navbar scrolled":"navbar"}>
       <div className="logo-wrapper">
-        <img src={logo} alt="kartikey-logo" />
+        <img src={logo} alt="kartikey-logo" onClick={()=>{window.location.href = "#"}}/>
       </div>
-      <div className="list-wrapper">
-        <span>Home</span>
-        <span>About me</span>
-        <span>Projects</span>
-        <span>Resume</span>
+      <div className={toggled ? "list-wrapper" : "list-wrapper off"}>
+        <span onClick={()=>{window.location.href = "#"}}>Home</span>
+        <span onClick={()=>{window.location.href = "#"}}>About me</span>
+        <span onClick={()=>{window.location.href = "#"}}>Projects</span>
+        <span onClick={()=>{window.location.href = "#"}}>Resume</span>
+      </div>
+      <div className="nav-toggler" onClick={()=>{setToggle(!toggled)}}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
       </div>
     </nav>
   );
